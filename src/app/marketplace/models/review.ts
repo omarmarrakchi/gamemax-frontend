923478ede3f4e0503df7613665e2da26415dfe9f
omar marrakchi <marrakchi.omar@esprit.tn>
2025-04-11 01:24:02 +0100

    add user data

--- src/app/marketplace/models/review.ts
+++ src/app/marketplace/models/review.ts
@@ -1,13 +1,19 @@
+import {User} from "./user";
+
 export class Review {
   reviewId?: number;
   reviewText: string;
   rating: number;
   reviewDate: Date;
+  userId: number;
+  user: User
 
-  constructor(reviewText: string, rating: number, reviewId?: number) {
+  constructor(reviewText: string, rating: number, reviewId?: number, user?: User, userId?: number) {
     this.reviewId = reviewId;
     this.reviewText = reviewText;
     this.rating = rating;
     this.reviewDate = new Date();
+    this.userId = userId || 0;
+    this.user = user || new User(0, '', '', '', '', '', false, '', '');
   }
 }
